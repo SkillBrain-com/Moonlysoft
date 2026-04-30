c4663406a9a12c05f1a9429ea53d1f41bcae4148
Madalina Samoila <mada.samoila123@gmail.com>
2026-04-30 20:16:23 +0300

    adding log out function

--- src/test/java/steps/HomePageStepDefinitions.java
+++ src/test/java/steps/HomePageStepDefinitions.java
@@ -1,27 +1,72 @@
 package steps;
 
-import config.EnvironmentConfig;
+
+import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
 import pages.HomePage;
-
-import static org.assertj.core.api.Assertions.assertThat;
 
 public class HomePageStepDefinitions {
 
     private HomePage homePage;
 
-     @Given("I open the application home page")
-    public void iOpenTheApplicationHomePage() {
-        String baseUrl = EnvironmentConfig.getBaseUrl();
-        homePage.openHomePage(baseUrl);
+    @Given("the REGULAR user is on the dashboard")
+    public void theREGULARUserIsOnTheDashboard() {
     }
 
-    @Then("the page title is not empty")
-    public void thePageTitleIsNotEmpty() {
-        String title = homePage.getDriver().getTitle();
-        assertThat(title)
-                .as("Expected page title to be non-empty for env: %s", EnvironmentConfig.getActiveEnvironment())
-                .isNotBlank();
+    @And("the user clicks on {string}")
+    public void theUserClicksOn(String arg0) {
+    }
+
+    @When("the user fills all required fields in the new case form")
+    public void theUserFillsAllRequiredFieldsInTheNewCaseForm() {
+    }
+
+    @And("selects experts")
+    public void selectsExperts() {
+    }
+
+    @And("uploads valid files")
+    public void uploadsValidFiles() {
+    }
+
+    @Then("the case should be created successfully")
+    public void theCaseShouldBeCreatedSuccessfully() {
+    }
+
+    @Given("the REGULAR user is creating a case")
+    public void theREGULARUserIsCreatingACase() {
+    }
+
+    @When("required fields are missing")
+    public void requiredFieldsAreMissing() {
+    }
+
+    @Then("validation errors should be displayed")
+    public void validationErrorsShouldBeDisplayed() {
+    }
+
+    @And("the case should not be created")
+    public void theCaseShouldNotBeCreated() {
+    }
+
+    @And("user completes log out process")
+    public void userCompletesLogOutProcess() {
+        homePage.clickLogout();
+    }
+
+
+    @And("I change language to {string}")
+    public void iChangeLanguageTo(String language) {
+        homePage.switchLanguage(language);
+    }
+
+    @And("I check user is logged out")
+    public void iCheckUserIsLoggedOut() {
+
+
     }
 }
+
+
--- src/test/java/steps/LoginPageStepDefinition.java
+++ src/test/java/steps/LoginPageStepDefinition.java
@@ -3,6 +3,7 @@ package steps;
 import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
 import pages.LoginPage;
 
 public class LoginPageStepDefinition {
@@ -44,6 +45,47 @@ public class LoginPageStepDefinition {
     @Then("I check error message is displayed")
     public void iCheckErrorMessageIsDisplayed() {
         loginPage.checkPasswordError();
+    }
+
+    @Given("the user is logged in as REGULAR")
+    public void theUserIsLoggedInAsREGULAR() {
+
+    }
+
+    @When("the dashboard page is loaded")
+    public void theDashboardPageIsLoaded() {
+    }
+
+    @Then("only Dashboard, Patient and Profile menu items should be visible")
+    public void onlyDashboardPatientAndProfileMenuItemsShouldBeVisible() {
+    }
+
+    @And("Requests, Admin and Analytics menu items should not be visible")
+    public void requestsAdminAndAnalyticsMenuItemsShouldNotBeVisible() {
+    }
+
+    @When("the user navigates to the admin page")
+    public void theUserNavigatesToTheAdminPage() {
+    }
+
+    @Then("access should be denied")
+    public void accessShouldBeDenied() {
+    }
+
+    @And("the user should not see the admin panel content")
+    public void theUserShouldNotSeeTheAdminPanelContent() {
+    }
+
+    @And("the user role is changed to EXPERT in the system")
+    public void theUserRoleIsChangedToEXPERTInTheSystem() {
+    }
+
+    @When("the user refreshes the application")
+    public void theUserRefreshesTheApplication() {
+    }
+
+    @Then("the user should see EXPERT-specific menu items")
+    public void theUserShouldSeeEXPERTSpecificMenuItems() {
     }
 }
 
